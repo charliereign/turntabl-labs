f371f3f5b686e9e4f7e0faed63a84e06508c3652
charliescode <charles.gold@turntabl.io>
2021-09-27 15:53:23 +0000

    Initialized this project with git

---
+++ intro.cpp
@@ -0,0 +1,102 @@
+#include<iostream>
+
+using namespace std;
+
+//TODO: Forward Declaration
+// int num();
+
+//TODO: Call by Pointer
+
+//TODO: FACTORIAL
+int factorial(int n){
+    if (n == 0)
+    {
+        return 1;
+    }
+    int facto = 1;
+    for (int i = 1; i <= n; i++)
+    {
+       facto *= i;
+    }
+    return facto;
+}
+
+// void swap(int *a, int *b){
+//     int temp = *a;
+//     *a = *b;
+//     *b = temp;
+// }
+
+int main(int argc, char const *argv[])
+{
+    // int x = 5, y = 6;
+    // cout << x << " / " << y << endl;
+    // swap(&x, &y);
+    // cout << x << " / "<< y <<endl;
+
+    cout <<factorial(3);
+
+    // num();
+    // int a, b;
+    // a = 10; b= 5;
+    // a > b ? cout <<"Yes a is greater than b"<<endl : cout<<"A is not greater than B"<<endl;
+
+    //TODO: POINTER
+    // int var;
+    // int *varptr;
+
+    // var = 5837;
+    // varptr = &var;
+
+    // cout << var <<endl;
+    // cout << &varptr <<endl;
+
+    //TODO: REFERENCE TYPE
+
+    // int i = 5;
+    // int &ir = i;
+    // cout<<i<<endl;
+    // cout<<ir<<endl;
+
+    //TODO: CHARACTER ARRAYS
+    // char name[] = "charles";
+    // int i = 0;
+    // while (name[i] != '\0')
+    // {
+    //     cout << "The characters are "<<name[i]<<endl;
+    //     i++;
+    // }
+
+    // int name = {7};
+    // cout<<name<<endl;
+
+    // int num = 8;
+    // num++;
+    // cout<<num;
+
+    // int num = 8;
+    // // cout << ++num;
+    // ++num;
+    // cout<<num;
+
+    //TODO: Range for statement
+
+    // int num[] = {1,2,3,4,5};
+    // for (int i : num)
+    // {
+    //     cout <<i<<endl;
+    // }
+
+    
+
+    
+}
+
+//TODO: Forward Declaration
+// int num(){
+//     int num;
+//     cout <<"Enter a number"<<endl;
+//     cin>>num;
+//     cout<<"the number you entered is "<<num<<endl;
+//     return num;
+// }
