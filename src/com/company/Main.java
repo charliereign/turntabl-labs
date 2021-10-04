8323af065e533e58683da5c08a567674c2da9707
charliescode <charles.gold@turntabl.io>
2021-10-04 15:40:42 +0000

    Changes by Joseph

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,19 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Trade trade = new Trade("T2","NASDAQ",200, -29);
-        System.out.println(trade);
-        System.out.println(trade.getPrice());
+        Trade trade = new Trade("T2","NASDAQ",200, 2);
+//        System.out.println(trade);
+//        System.out.println(trade.getPrice());
+
+        Account account = new Account("12345", 2.7);
+
+        System.out.println("Before trade:");
+        Trader trader = new Trader("Bright & Sons", account);
+        System.out.println(trader);
+
+        System.out.println("After trade:");
+        trader.addTrade(trade.getPrice(), trade.getQuantity());
+
+        System.out.println(trader);
     }
 }
---
+++ test/com/company/TraderTest.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class TraderTest {
+    //Account joe = new Account();
+    //Trader trader = new Trader("Joseph", joe);
+    @Test
+    @DisplayName("Should add value of trade to existing total trade value")
+    public void confirmTradeValueAddition(){
+//        assertTrue();
+    }
+}
